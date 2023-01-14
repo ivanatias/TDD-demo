4acb75e70d7a46b8811f28f5d18600b25d9497b4
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-14 18:42:26 -0400

    test(App): check if title is correctly rendered

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react'
 import App from '@/App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('<App />', () => {
+  // Test #1 - Check if the app's title is properly rendered then proceed to make the test pass.
+  it('correctly renders the app title', () => {
+    render(<App />)
+    const title = screen.getByRole('heading', { name: /products tdd /i })
+    expect(title).toBeInTheDocument()
+  })
 })
