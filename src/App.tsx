e608032eb49b0d5a008282ab9e94421b240c68d3
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-14 20:33:25 -0400

    feat(App): add products

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, ReactElement } from 'react'
+import { mockedProducts } from '@/tests/utils/mocked-products'
 
 const App = (): ReactElement => {
   const [loading, setLoading] = useState(true)
@@ -14,7 +15,28 @@ const App = (): ReactElement => {
       {/* Test #1 */}
       <h1>Products TDD Demo</h1>
       {/* Test #2 */}
-      {loading && <div role='progressbar'>Loading...</div>}
+      {loading ? (
+        <div role='progressbar'>Loading...</div>
+      ) : (
+        <ul>
+          {/* Test #3 */}
+          {mockedProducts.products.map(
+            ({ title, price, id, description, category, thumbnail }) => (
+              <li key={id}>
+                <div>
+                  <img src={thumbnail} alt={`product thumbnail-${id}`} />
+                  <h3>{title}</h3>
+                  <p>{description}</p>
+                  <p>{category}</p>
+                  <span>
+                    <strong>{price}</strong>
+                  </span>
+                </div>
+              </li>
+            )
+          )}
+        </ul>
+      )}
     </>
   )
 }
