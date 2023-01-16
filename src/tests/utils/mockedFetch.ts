2c51abd5eccf4f61b61016dc189b2d96410f7817
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-16 17:48:26 -0400

    feat: add mocked fetch implementation

---
+++ src/tests/utils/mockedFetch.ts
@@ -0,0 +1,39 @@
+import type { ProductsResponse } from '@/models/products'
+import { mockedProducts } from '@/tests/utils/mocked-products'
+
+type RequestMethod = 'POST' | 'DELETE' | 'PUT'
+
+interface RequestHeaders {
+  'Content-Type': string
+}
+
+interface RequestConfig {
+  method: RequestMethod
+  headers: RequestHeaders
+  body: string
+}
+
+interface MockedResults<T> {
+  ok: boolean
+  status: number
+  json: () => Promise<T>
+}
+
+const mockedFetch = (
+  url: string,
+  config?: RequestConfig
+): MockedResults<ProductsResponse> => {
+  switch (url) {
+    case 'https://dummyjson.com/products': {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => mockedProducts
+      }
+    }
+    default:
+      throw new Error(`Unhandled request: ${url}`)
+  }
+}
+
+export { mockedFetch }
