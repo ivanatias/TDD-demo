0622e6179350482351c63978f7cef923d77985cc
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-16 21:01:19 -0400

    feat: add mock server config

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,8 @@
+import { server } from './mocks/server.js'
 import '@testing-library/jest-dom'
+
+beforeAll(() => server.listen())
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
